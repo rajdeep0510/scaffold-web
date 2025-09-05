d6d7bacc6a255d2ddda3abd41aa8e0f532ee3494
pratham2008 <patel.pratham.1605@gmail.com>
2025-09-06 00:12:47 +0530

    scaffold 1.2.1.2

---
+++ app/components/skeleton/page.js
@@ -0,0 +1,16 @@
+import { Skeleton } from "./index";
+
+export default function SkeletonPage() {
+  return (
+    <div className="p-10 space-y-6">
+      <h1 className="text-3xl font-bold">Skeleton Component</h1>
+      <p className="text-gray-400">Different variants of the Skeleton component</p>
+
+      <div className="space-y-4">
+        <Skeleton variant="text" />
+        <Skeleton variant="circle" />
+        <Skeleton variant="block" />
+      </div>
+    </div>
+  );
+}
